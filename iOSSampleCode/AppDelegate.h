466fe6315591eb26c3258016dd5f36d4748813a9
zhongwu <zhongwuzw@qq.com>
2016-06-24 13:18:05 +0800

    Initial Commit

---
+++ iOSSampleCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSSampleCode
+//
+//  Created by 钟武 on 16/6/24.
+//  Copyright © 2016年 钟武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
